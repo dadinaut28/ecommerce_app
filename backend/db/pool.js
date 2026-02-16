5d5b70baa964ccfa4d2c65f23a0e1ffe59179e32
Dadinaut <dadinaut@gmail.com>
2026-02-16 13:11:38 +0100

    pool reconfigured

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -14,7 +14,10 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
